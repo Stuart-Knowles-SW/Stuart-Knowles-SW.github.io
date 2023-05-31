e044ec18b312f4ee6fca66614ed044d485f31c87
Stuart Knowles <Stuart.Knowles@softwire.com>
2023-05-31 13:35:53 +0100

    Moar website

--- app.js
+++ app.js
@@ -1,8 +1,10 @@
 const express = require("express");
+const favicon = require("serve-favicon")
 const app = express();
 app.get("/", function (req, res) {
     res.sendFile(__dirname + "/index.html");
 });
+app.use(favicon("./images/myIcon.ico"));
 app.use('/scripts', express.static('scripts'))
 app.use('/content', express.static('content'))
 app.use('/images', express.static('images'))
